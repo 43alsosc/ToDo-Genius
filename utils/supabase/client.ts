1cf83ba6f08f2445ebe232394823af9aea6ad345
43alsosc <43alsosc@stud.akademiet.no>
2024-04-15 09:05:22 +0200

    funker dette?

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,21 +1,15 @@
-import { createBrowserClient } from "@supabase/ssr";
-import { useMemo } from "react";
+import { useMemo } from 'react';
+import { createBrowserClient } from '@supabase/ssr';
 
 export function getSupabaseBrowserClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 }
 
 function useSupabaseClient() {
-  return useMemo(getSupabaseBrowserClient, [])
+  return useMemo(getSupabaseBrowserClient, []);
 }
-
-export const createClient = () =>
-  createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  );
 
 export default useSupabaseClient;
