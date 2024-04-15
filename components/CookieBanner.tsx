d3fe3cf83c7858a27808696d3f1157c374b9c83b
43alsosc <43alsosc@stud.akademiet.no>
2024-04-15 11:02:09 +0200

    Nettleseren husker at den har blitt lukket

--- components/CookieBanner.tsx
+++ components/CookieBanner.tsx
@@ -1,11 +1,35 @@
-// CookieBanner.tsx
-import React from "react";
-import styles from "./CookieBanner.module.css"; // Assuming you're using CSS modules
+"use client";
+import React, { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+import { Close } from "@mui/icons-material";
 
 const CookieBanner: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const cookieBannerClosed = localStorage.getItem("cookieBannerClosed");
+    if (cookieBannerClosed === "true") {
+      setIsVisible(false);
+    }
+  }, []);
+
+  const closeBanner = () => {
+    setIsVisible(false);
+    localStorage.setItem("cookieBannerClosed", "true");
+  };
+
+  if (!isVisible) {
+    return null;
+  }
+
   return (
     <div className="fixed bottom-5 right-5 w-80 bg-stone-700 p-5 rounded-xl shadow-m z-50 text-white">
-      <h2>Cookies on Our Site</h2>
+      <div className="flex justify-between">
+        <h2 className="py-2 ">Cookies on Our Site</h2>
+        <Button className="bg-transparent" onClick={closeBanner}>
+          <Close />
+        </Button>
+      </div>
       <p>
         We use cookies to improve your browsing experience. By using our site,
         you agree to our use of cookies.
