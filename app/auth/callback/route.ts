09b46b270e28e1aa82fadcd20eef613e92d6b2b0
43alsosc <43alsosc@stud.akademiet.no>
2024-04-15 09:40:46 +0200

    DONE

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const code = searchParams.get('code');
 
-  const origin = process.env.ORIGIN || 'localhost:3000';
+  const origin = process.env.ORIGIN || 'http://todogenuis.duckdns.org';
 
   if (code) {
     const cookieStore = cookies();
