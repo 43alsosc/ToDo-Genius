4598955d3ec1575632fc7d11e12ffe2e1fd8481a
43alsosc <43alsosc@stud.akademiet.no>
2024-04-18 15:01:27 +0200

    Add getData function to fetch data from Supabase API

---
+++ api/fetch/getDataTableData.tsx
@@ -0,0 +1,16 @@
+import { ToDo } from "@/components/data-table/columns";
+import { createClient } from "@/utils/supabase/server";
+
+export default async function getData(): Promise<ToDo[]> {
+  const supabase = createClient();
+  supabase.auth.getUser();
+
+  const { data, error } = await supabase.from("todos").select("*");
+  if (error) {
+    console.error("Error fetching todos", error);
+    throw error;
+  } else if (!data) {
+    console.error("No data found");
+  }
+  return data;
+}
