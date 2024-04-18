4a278eb8cacd8b353778d9a868ae72ae31b571d9
43alsosc <43alsosc@stud.akademiet.no>
2024-04-18 13:32:05 +0200

    Update side-nav links

--- components/side-nav.tsx
+++ components/side-nav.tsx
@@ -14,7 +14,7 @@ export function SideNav() {
     <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background md:flex">
       <nav className="flex flex-col items-center gap-4 px-2 md:py-5 ">
         <Link
-          href="#"
+          href="/dashboard"
           className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
         >
           <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
@@ -38,7 +38,7 @@ export function SideNav() {
           <Tooltip>
             <TooltipTrigger asChild>
               <Link
-                href="/settings"
+                href="/dashboard/settings"
                 className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
               >
                 <Settings className="h-5 w-5" />
@@ -80,7 +80,7 @@ export function CollapsedSideNav() {
               Dashboard
             </Link>
             <Link
-              href="/settings"
+              href="/dashboard/settings"
               className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
             >
               <Settings className="h-5 w-5" />
