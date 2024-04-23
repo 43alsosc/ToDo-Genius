ff9d9ad3e22db660779a83e4766748bff803c14d
43alsosc <43alsosc@stud.akademiet.no>
2024-04-23 10:27:13 +0200

    hvis bruker ikke er loget inn redirect til /login

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -12,8 +12,20 @@ import { Input } from "@/components/ui/input";
 import { CollapsedSideNav } from "@/components/side-nav";
 import Profile from "@/components/Profile";
 import ModeToggle from "@/components/DarkmodeButton";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+import { DeleteUser, DeleteUserButton } from "@/components/AuthButton";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/login");
+  }
   return (
     <div className="flex min-h-screen w-full flex-col">
       <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
@@ -57,6 +69,15 @@ export default function Dashboard() {
                 <form>
                   <ModeToggle />
                 </form>
+              </CardContent>
+            </Card>
+            <Card x-chunk="dashboard-04-chunk-1">
+              <CardHeader>
+                <CardTitle>Delete Account</CardTitle>
+                <CardDescription>Delete your account</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <DeleteUser />
               </CardContent>
             </Card>
           </div>
