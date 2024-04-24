8c62ec157a3700d6ac5d6eef478584a58ac5905f
43alsosc <43alsosc@stud.akademiet.no>
2024-04-24 10:21:04 +0200

    changed todo to be input and add button

--- components/AddToDo.tsx
+++ components/AddToDo.tsx
@@ -1,35 +1,23 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import Modal from "react-modal";
+import React, { useState } from "react";
 import { Button } from "./ui/button";
 import { PlusCircle } from "lucide-react";
 import { Input } from "./ui/input";
 import { createClient } from "@/utils/supabase/client";
 
 export default function AddProduct() {
-  useEffect(() => {
-    Modal.setAppElement("#root");
-  }, []);
-
   const supabase = createClient();
 
   const [task, setTask] = useState("");
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const openModal = () => {
-    setIsModalOpen(true);
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTask(event.target.value);
   };
 
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
-  // This line is important for accessibility reasons. Replace '#root' with the id of your app's root element.
-
-  const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  const handleFormSubmit = async (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
     event.preventDefault();
-
-    const supabase = createClient();
 
     const {
       data: { user },
@@ -63,60 +51,31 @@ export default function AddProduct() {
       }
       console.log("Form submitted");
     }
-    closeModal();
+    setTask("");
   };
 
   return (
     <div>
-      <Button
-        size="sm"
-        variant="outline"
-        className="h-10 gap-1"
-        onClick={openModal}
-      >
-        <PlusCircle className="h-3.5 w-3.5" />
-        <span>Add ToDo</span>
-      </Button>
-      <Modal
-        isOpen={isModalOpen}
-        onRequestClose={closeModal}
-        style={{
-          content: {
-            top: "50%",
-            left: "50%",
-            right: "auto",
-            bottom: "auto",
-            marginRight: "-50%",
-            transform: "translate(-50%, -50%)",
-            width: "90%",
-            maxWidth: "500px",
-            padding: "1rem",
-            borderRadius: "0.5rem",
-            boxShadow: "0 25px 50px -12px rgba(0, 0, 0, 0.25)",
-            border: "none",
-            background: "white",
-            color: "white",
-          },
-        }}
-      >
-        <form onSubmit={handleFormSubmit}>
-          {/* Your form fields go here. */}
-          <div className="flex">
-            <Input
-              type="text"
-              placeholder="Add ToDo (min 4 characters)"
-              value={task}
-              onChange={(e) => setTask(e.target.value)}
-            />
-            <Button type="submit">
-              <PlusCircle className="h-3.5 w-3.5" />
-              <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                Add ToDo
-              </span>
-            </Button>
-          </div>
-        </form>
-      </Modal>
+      <form className="flex-1">
+        <div className="flex items-center">
+          <Input
+            className="min-w-0 w-full"
+            placeholder="Add new todo..."
+            type="text"
+            value={task}
+            onChange={handleInputChange}
+          />
+          <Button
+            size="sm"
+            variant="outline"
+            className="h-10 gap-1"
+            onClick={handleFormSubmit}
+          >
+            <PlusCircle className="h-3.5 w-3.5" />
+            <span>Add ToDo</span>
+          </Button>
+        </div>
+      </form>
     </div>
   );
 }
