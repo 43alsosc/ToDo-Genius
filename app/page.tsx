b03aa4daf1ca94558b81d132980a95208e3c9fbd
43alsosc <43alsosc@stud.akademiet.no>
2024-04-24 09:54:59 +0200

    Refactor page.tsx to update layout and components

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,9 @@
-import { Button } from "@/components/ui/button";
-import AuthButton from "../components/AuthButton";
-import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
-import { Link } from "lucide-react";
+import { LandingPageComponent } from "@/components/landing-page-component";
 
-export default async function Index() {
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
-
-  const isSupabaseConnected = canInitSupabaseClient();
-
+export default async function Page() {
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          {isSupabaseConnected && <AuthButton />}
-        </div>
-      </nav>
-
-      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <main className="flex-1 flex flex-col gap-6">
-          {isSupabaseConnected && (
-            <Link href="/dashboard" className="">
-              Dashboard
-            </Link>
-          )}
-        </main>
-      </div>
+    <div className="flex-1 min-h-[100dvh] min-w-screen flex flex-col gap-20 items-center">
+      <LandingPageComponent />
     </div>
   );
 }
